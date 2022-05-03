f85781564dd2ef9e0b01ae57c1853039eab91a0e
Elton Bismarck <94170844+Elton-Bismarck@users.noreply.github.com>
2022-05-03 09:43:35 -0300

    Add files via upload

---
+++ Questão 6.c
@@ -0,0 +1,4 @@
+A primeira expressão;
+O operador * leva a expressão a apontar para o conteudo do endereço do elemento do array, somado a 2, ele aponta para o terceiro elemento.
+A segunda expressão aponta para o quinto elemento;
+A terceira e a quarta expressão aponta para o endereço onde está o quinto e o terceiro elemento do array.
